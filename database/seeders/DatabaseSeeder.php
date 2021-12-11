4e0e6ed66bf1989f6f711b6f6eec4f22d3787cdc
jonathantyar <jonathantyar@gmail.com>
2021-12-11 17:59:00 +0700

    Added seeder for creating investment product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\InvestmentProduct::factory(1)->create([
+            'code' => 'ib',
+            'name' => 'bitcoin'
+        ]);
     }
 }
