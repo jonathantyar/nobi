36ca0ba7c4a0b704039af03caf7086d1a4abfac5
jonathantyar <jonathantyar@gmail.com>
2021-12-12 01:56:36 +0700

    Added Test for list nab and order

--- tests/Feature/UpdateTotalBalanceTest.php
+++ tests/Feature/UpdateTotalBalanceTest.php
@@ -4,6 +4,7 @@ use App\Models\User;
 use App\Models\InvestmentProduct;
 
 use function Pest\Laravel\post;
+use function Pest\Laravel\get;
 
 /**
  * Test if investment product code does not exists.
@@ -43,7 +44,7 @@ test('Update total balance when unit zero', function () {
     $investment = InvestmentProduct::factory()->create();
 
     $response = post(route('investment.update.balance', ['investment_product' => $investment->code]), [
-        'current_balance' => 100000
+        'current_balance' => 100000.05
     ]);
     $response->assertStatus(200);
     expect($response->json())->toMatchArray([
@@ -60,14 +61,14 @@ test('Update total balance when unit not zero', function () {
     $user = User::factory()->create();
     $investment = InvestmentProduct::factory()->hasAttached(
         $user,
-        ['balance' => 2500000.25, 'unit' => 1500.0125]
+        ['unit' => 1500.0125]
     )->create();
 
     /**
      * 10000 / 1500.0125 = 6.6666
      */
     $response = post(route('investment.update.balance', ['investment_product' => $investment->code]), [
-        'current_balance' => 10000
+        'current_balance' => 10000.05
     ]);
     $response->assertStatus(200);
     expect($response->json())->toMatchArray([
@@ -84,17 +85,59 @@ test('Update total balance when unit not zero check round down', function () {
     $user = User::factory()->create();
     $investment = InvestmentProduct::factory()->hasAttached(
         $user,
-        ['balance' => 2500000.25, 'unit' => 5250.0125]
+        ['unit' => 5250.0125]
     )->create();
 
     /**
-     * 10000 / 1500.0125 = 6.6666
+     * 10000.05 / 5250.0125 = 1.9047
      */
     $response = post(route('investment.update.balance', ['investment_product' => $investment->code]), [
-        'current_balance' => 10000
+        'current_balance' => 10000.05
     ]);
     $response->assertStatus(200);
     expect($response->json())->toMatchArray([
+        'nab' => 1.9047
+    ]);
+});
+
+/**
+ * Test get the lastest nab from list.
+ *
+ * @return void
+ */
+test('Update total balance and list get the lastest', function () {
+    $user = User::factory()->create();
+    $investment = InvestmentProduct::factory()->hasAttached(
+        $user,
+        ['unit' => 5250.0125]
+    )->create();
+
+    /**
+     * 12000.05 / 5250.0125 = 2.2857
+     */
+    $response = post(route('investment.update.balance', ['investment_product' => $investment->code]), [
+        'current_balance' => 12000.05
+    ]);
+    $response->assertStatus(200);
+    expect($response->json())->toMatchArray([
+        'nab' => 2.2857
+    ]);
+
+    /**
+     * 10000.05 / 5250.0125 = 1.9047
+     */
+    $response = post(route('investment.update.balance', ['investment_product' => $investment->code]), [
+        'current_balance' => 10000.05
+    ]);
+    $response->assertStatus(200);
+    expect($response->json())->toMatchArray([
+        'nab' => 1.9047
+    ]);
+
+
+    $response = get(route('investment.list.nab', ['investment_product' => $investment->code]), []);
+    $response->assertStatus(200);
+    expect($response->json()[0])->toMatchArray([
         'nab' => 1.9047
     ]);
 });
