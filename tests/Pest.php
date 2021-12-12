1c63ed822595fcbb824b107f4b05785831a42efc
jonathantyar <jonathantyar@gmail.com>
2021-12-12 11:48:40 +0700

    Added Calculate test for roundown helper

--- tests/Pest.php
+++ tests/Pest.php
@@ -15,7 +15,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(Tests\TestCase::class)->in('Feature');
 uses(RefreshDatabase::class)->in('Feature');
-uses(RefreshDatabase::class)->in('Unit');
 
 /*
 |--------------------------------------------------------------------------
--- tests/Unit/UpdateTotalBalance.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-test('Update database balance', function () {
-    expect();
-});
